b7b84c950a01902974960b56e5c87e947786c8bd
Sjoerd Scheffer <sjoerd@b00n.org>
2022-02-24 23:40:59 +0100

    Added integration tests for solo buttons

--- test/integration/ApplicationContainer.h
+++ test/integration/ApplicationContainer.h
@@ -17,7 +17,7 @@
 #include "../../src/di.h"
 #include "../fakes/LibraryAbstractions/Curl/Factories/CurlAbstractionFactoryFake.h"
 #include "../fakes/LibraryAbstractions/RtMidi/Factories/RtMidiAbstractionFactoryFake.h"
-#include <vector>
+#include <memory>
 
 namespace MackieOfTheUnicorn::Tests::Integration
 {
@@ -31,11 +31,7 @@ namespace MackieOfTheUnicorn::Tests::Integration
 		    MackieOfTheUnicorn::Application& application,
 		    MackieOfTheUnicorn::LibraryAbstractions::Curl::Factories::CurlAbstractionFactory& curlAbstractionFactory,
 		    MackieOfTheUnicorn::LibraryAbstractions::RtMidi::Factories::RtMidiAbstractionFactory&
-		        rtMidiAbstractionFactory)
-		    : Application(&application), CurlAbstractionFactory(&curlAbstractionFactory),
-		      RtMidiAbstractionFactory(&rtMidiAbstractionFactory)
-		{
-		}
+		        rtMidiAbstractionFactory);
 	};
 
 	struct ApplicationContainerWrapper
@@ -51,55 +47,10 @@ namespace MackieOfTheUnicorn::Tests::Integration
 		    LibraryAbstractions::RtMidi::RtMidiInAbstractionFake* rtMidiInAbstractionFake,
 		    LibraryAbstractions::RtMidi::RtMidiOutAbstractionFake* rtMidiOutAbstractionFake,
 		    LibraryAbstractions::Curl::CurlAbstractionFake* curlInAbstractionFake,
-		    LibraryAbstractions::Curl::CurlAbstractionFake* curlOutAbstractionFake)
-		    : ApplicationContainer(applicationContainer), RtMidiInAbstractionFake(rtMidiInAbstractionFake),
-		      RtMidiOutAbstractionFake(rtMidiOutAbstractionFake), CurlInAbstractionFake(curlInAbstractionFake),
-		      CurlOutAbstractionFake(curlOutAbstractionFake)
-		{
-		}
+		    LibraryAbstractions::Curl::CurlAbstractionFake* curlOutAbstractionFake);
 	};
 
-	auto GetApplicationContainerWrapper()
-	{
-		auto injector = di::make_injector(
-		    di::bind<MackieOfTheUnicorn::Mackie::MackieService>.to<MackieOfTheUnicorn::Mackie::MackieServiceImpl>(),
-		    di::bind<MackieOfTheUnicorn::Mackie::Factories::MackieCompositeFactory>.to<MackieOfTheUnicorn::Mackie::Factories::MackieCompositeFactoryImpl>(),
-		    di::bind<MackieOfTheUnicorn::MIDI::MIDIService>.to<MackieOfTheUnicorn::MIDI::MIDIServiceImpl>(),
-		    di::bind<MackieOfTheUnicorn::MIDI::Factories::MIDIDeviceFactory>.to<MackieOfTheUnicorn::MIDI::Factories::MIDIDeviceFactoryImpl>(),
-		    di::bind<MackieOfTheUnicorn::LibraryAbstractions::RtMidi::Factories::RtMidiAbstractionFactory>.to<MackieOfTheUnicorn::LibraryAbstractions::RtMidi::Factories::RtMidiAbstractionFactoryFake>(),
-		    di::bind<MackieOfTheUnicorn::Mackie::Factories::MackieDeviceFactory>.to<MackieOfTheUnicorn::Mackie::Factories::MackieDeviceFactoryImpl>(),
-		    di::bind<MackieOfTheUnicorn::HTTP::Factories::HTTPDeviceFactory>.to<MackieOfTheUnicorn::HTTP::Factories::HTTPDeviceFactoryImpl>(),
-		    di::bind<MackieOfTheUnicorn::LibraryAbstractions::Curl::Factories::CurlAbstractionFactory>.to<MackieOfTheUnicorn::LibraryAbstractions::Curl::Factories::CurlAbstractionFactoryFake>());
-		auto applicationContainer = injector.create<MackieOfTheUnicorn::Tests::Integration::ApplicationContainer>();
-
-		auto rtMidiAbstractionFactoryFake =
-		    dynamic_cast<LibraryAbstractions::RtMidi::Factories::RtMidiAbstractionFactoryFake*>(
-		        applicationContainer.RtMidiAbstractionFactory);
-		auto curlAbstractionFactoryFake =
-		    dynamic_cast<LibraryAbstractions::Curl::Factories::CurlAbstractionFactoryFake*>(
-		        applicationContainer.CurlAbstractionFactory);
-
-		auto rtMidiInAbstractionFake = *(rtMidiAbstractionFactoryFake->ReturnedInAbstractions.end() - 1);
-		auto rtMidiOutAbstractionFake = *(rtMidiAbstractionFactoryFake->ReturnedOutAbstractions.end() - 1);
-
-		rtMidiInAbstractionFake->PortCount = 1;
-		rtMidiInAbstractionFake->PortName = "input1";
-		rtMidiOutAbstractionFake->PortCount = 1;
-		rtMidiOutAbstractionFake->PortName = "output1";
-
-		std::vector<std::pair<int, int>> inputAndOutputIds = {{0, 0}};
-		applicationContainer.Application->Start(inputAndOutputIds);
-
-		rtMidiInAbstractionFake = *(rtMidiAbstractionFactoryFake->ReturnedInAbstractions.end() - 1);
-		rtMidiOutAbstractionFake = *(rtMidiAbstractionFactoryFake->ReturnedOutAbstractions.end() - 1);
-
-		auto curlInAbstractionFake = *(curlAbstractionFactoryFake->CreateCurlAbstractionFakes.end() - 2);
-		auto curlOutAbstractionFake = *(curlAbstractionFactoryFake->CreateCurlAbstractionFakes.end() - 1);
-
-		return std::make_unique<ApplicationContainerWrapper>(applicationContainer, rtMidiInAbstractionFake,
-		                                                     rtMidiOutAbstractionFake, curlInAbstractionFake,
-		                                                     curlOutAbstractionFake);
-	}
+	std::unique_ptr<ApplicationContainerWrapper> GetApplicationContainerWrapper();
 } // namespace MackieOfTheUnicorn::Tests::Integration
 
 #endif // MACKIE_OF_THE_UNICORN_APPLICATIONCONTAINER_H
--- test/integration/ChannelSolosTest.cpp
+++ test/integration/ChannelSolosTest.cpp
@@ -1,14 +1,13 @@
 //
-// Created by Sjoerd Scheffer on 20/02/2022.
+// Created by Sjoerd Scheffer on 24/02/2022.
 //
-
 #include "ApplicationContainer.h"
 #include "gtest/gtest.h"
 #include <thread>
 
 namespace MackieOfTheUnicorn::Tests::Integration
 {
-	class ChannelMutesTest : public ::testing::Test
+	class ChannelSolosTest : public ::testing::Test
 	{
 	  protected:
 		void SetUp() override
@@ -32,15 +31,16 @@ namespace MackieOfTheUnicorn::Tests::Integration
 				auto curlIn = wrapper->CurlInAbstractionFake;
 				auto rtMidiOut = wrapper->RtMidiOutAbstractionFake;
 				std::ostringstream stringStream;
-				stringStream << "{\"mix/chan/" << i << "/matrix/mute\":" << (expectedOn ? "1.000000" : "0.000000")
-				             << "}";
+				stringStream << "{\"mix/chan/" << i << "/matrix/solo\":" << (expectedOn ? "1.000000" : "0.000000")
+							 << "}";
 				auto jsonMessage = stringStream.str();
 
-				std::vector<unsigned char> expectedMIDIMessage = {144, (unsigned char)(16 + i),
-				                                                  (unsigned char)(expectedOn ? 127 : 0)};
+				std::vector<unsigned char> expectedMIDIMessage = {144, (unsigned char)(8 + i),
+																  (unsigned char)(expectedOn ? 127 : 0)};
 
 				auto oldSize = rtMidiOut->SendMessageMessages.size();
 				curlIn->FakeHasChangedMessage(jsonMessage);
+
 				auto startTime = std::chrono::system_clock::now();
 				while (rtMidiOut->SendMessageMessages.size() == oldSize)
 				{
@@ -59,39 +59,39 @@ namespace MackieOfTheUnicorn::Tests::Integration
 		}
 	};
 
-	TEST_F(ChannelMutesTest, MackieCanSwitchFirst8Mutes)
+	TEST_F(ChannelSolosTest, MackieCanSwitchFirst8Mutes)
 	{
 		auto curlOut = wrapper->CurlOutAbstractionFake;
 		auto rtMidiIn = wrapper->RtMidiInAbstractionFake;
 
-		auto switchMute = [&curlOut, &rtMidiIn](int i, bool expectedOn) {
-			std::ostringstream stringStream;
-			stringStream << "json={\"mix/chan/" << i << "/matrix/mute\":" << (expectedOn ? 1 : 0) << "}";
-			auto expectedJSONMessage = stringStream.str();
+		auto switchSolo = [&curlOut, &rtMidiIn](int i, bool expectedOn) {
+		  std::ostringstream stringStream;
+		  stringStream << "json={\"mix/chan/" << i << "/matrix/solo\":" << (expectedOn ? 1 : 0) << "}";
+		  auto expectedJSONMessage = stringStream.str();
 
-			std::vector<unsigned char> message = {144, (unsigned char)(16 + i), 127};
-			rtMidiIn->FakeMessage(0, message);
-			message = {144, (unsigned char)(16 + i), 0};
-			rtMidiIn->FakeMessage(0, message);
+		  std::vector<unsigned char> message = {144, (unsigned char)(8 + i), 127};
+		  rtMidiIn->FakeMessage(0, message);
+		  message = {144, (unsigned char)(8 + i), 0};
+		  rtMidiIn->FakeMessage(0, message);
 
-			auto actualJSONMessage = curlOut->SetPostDataPostData;
+		  auto actualJSONMessage = curlOut->SetPostDataPostData;
 
-			EXPECT_EQ(actualJSONMessage, expectedJSONMessage);
+		  EXPECT_EQ(actualJSONMessage, expectedJSONMessage);
 		};
 
 		for (int i = 0; i < 8; i++)
 		{
-			switchMute(i, true);
-			switchMute(i, false);
+			switchSolo(i, true);
+			switchSolo(i, false);
 		}
 	}
 
-	TEST_F(ChannelMutesTest, MOTUCanSwitchFirst8ChannelsOn)
+	TEST_F(ChannelSolosTest, MOTUCanSwitchFirst8ChannelsOn)
 	{
 		MOTUCanSwitchFirst8Channels(true);
 	}
 
-	TEST_F(ChannelMutesTest, MOTUCanSwitchFirst8ChannelsOff)
+	TEST_F(ChannelSolosTest, MOTUCanSwitchFirst8ChannelsOff)
 	{
 		MOTUCanSwitchFirst8Channels(false);
 	}
