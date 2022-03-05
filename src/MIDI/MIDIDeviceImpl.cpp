fe69855ce196d88ab22b70a1c634c7ee41a8e6b1
Sjoerd Scheffer <sjoerd@b00n.org>
2022-03-05 16:42:21 +0100

    Made the MIDI message delay 1ms

--- src/MIDI/MIDIDeviceImpl.cpp
+++ src/MIDI/MIDIDeviceImpl.cpp
@@ -35,7 +35,7 @@ namespace MackieOfTheUnicorn::MIDI
 	{
 		RtMidiOut->SendMessage(&message);
 #if !TESTING
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 #endif
 	}
 }
